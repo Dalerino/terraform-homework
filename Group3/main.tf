e00c3c84da82618801d6ef8f3c5569721d9ebd13
Dalerino <baitursunov@gmail.com>
2024-07-27 19:24:55 +0000

    adjusted with new changes

--- Group3/main.tf
+++ Group3/main.tf
@@ -1,88 +1,3 @@
-# provider "aws" {
-#   region = var.region
-# }
-
-# #Create VPC
-# resource "aws_vpc" "group_3" {
-#   cidr_block = "10.0.0.0/16"
-
-#   tags = {
-#     Name = "group-3"
-#   }
-# }
-
-# # Key pair
-# resource "aws_key_pair" "deployer" {
-#   key_name   = "bastion"
-#   public_key = file("~/.ssh/id_rsa.pub")
-# }
-
-# #Create Subnets(3)
-# resource "aws_subnet" "public1" {
-#   vpc_id                  = aws_vpc.group_3.id
-#   cidr_block              = "10.0.1.0/24"
-#   availability_zone       = "us-east-2a"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "public1"
-#   }
-# }
-
-# resource "aws_subnet" "public2" {
-#   vpc_id                  = aws_vpc.group_3.id
-#   cidr_block              = "10.0.2.0/24"
-#   availability_zone       = "us-east-2b"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "public2"
-#   }
-# }
-
-# resource "aws_subnet" "public3" {
-#   vpc_id                  = aws_vpc.group_3.id
-#   cidr_block              = "10.0.3.0/24"
-#   availability_zone       = "us-east-2c"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "public3"
-#   }
-# }
-
-# # Create and attach Internet Gateway
-# resource "aws_internet_gateway" "group_3" {
-#   vpc_id = aws_vpc.group_3.id
-#   tags = {
-#     Name = "group_3"
-#   }
-# }
-
-# #Create route table
-# resource "aws_route_table" "group_3" {
-#   vpc_id = aws_vpc.group_3.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.group_3.id
-#   }
-#   tags = {
-#     Name = "group_3"
-#   }
-# }
-
-# #Associate subnets with route table
-# resource "aws_route_table_association" "a" {
-#   subnet_id      = aws_subnet.public1.id
-#   route_table_id = aws_route_table.group_3.id
-# }
-# resource "aws_route_table_association" "b" {
-#   subnet_id      = aws_subnet.public2.id
-#   route_table_id = aws_route_table.group_3.id
-# }
-# resource "aws_route_table_association" "c" {
-#   subnet_id      = aws_subnet.public3.id
-#   route_table_id = aws_route_table.group_3.id
-# }
-
-
 provider "aws" {
   region = var.region
 }
@@ -196,3 +111,90 @@ resource "aws_db_subnet_group" "group_3" {
     Name = "group-3"
   }
 }
+
+
+
+
+# provider "aws" {
+#   region = var.region
+# }
+
+# #Create VPC
+# resource "aws_vpc" "group_3" {
+#   cidr_block = "10.0.0.0/16"
+
+#   tags = {
+#     Name = "group-3"
+#   }
+# }
+
+# # Key pair
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "bastion"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
+
+# #Create Subnets(3)
+# resource "aws_subnet" "public1" {
+#   vpc_id                  = aws_vpc.group_3.id
+#   cidr_block              = "10.0.1.0/24"
+#   availability_zone       = "us-east-2a"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name = "public1"
+#   }
+# }
+
+# resource "aws_subnet" "public2" {
+#   vpc_id                  = aws_vpc.group_3.id
+#   cidr_block              = "10.0.2.0/24"
+#   availability_zone       = "us-east-2b"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name = "public2"
+#   }
+# }
+
+# resource "aws_subnet" "public3" {
+#   vpc_id                  = aws_vpc.group_3.id
+#   cidr_block              = "10.0.3.0/24"
+#   availability_zone       = "us-east-2c"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name = "public3"
+#   }
+# }
+
+# # Create and attach Internet Gateway
+# resource "aws_internet_gateway" "group_3" {
+#   vpc_id = aws_vpc.group_3.id
+#   tags = {
+#     Name = "group_3"
+#   }
+# }
+
+# #Create route table
+# resource "aws_route_table" "group_3" {
+#   vpc_id = aws_vpc.group_3.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.group_3.id
+#   }
+#   tags = {
+#     Name = "group_3"
+#   }
+# }
+
+# #Associate subnets with route table
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.public1.id
+#   route_table_id = aws_route_table.group_3.id
+# }
+# resource "aws_route_table_association" "b" {
+#   subnet_id      = aws_subnet.public2.id
+#   route_table_id = aws_route_table.group_3.id
+# }
+# resource "aws_route_table_association" "c" {
+#   subnet_id      = aws_subnet.public3.id
+#   route_table_id = aws_route_table.group_3.id
+# }
--- Group3/wordpress.sh
+++ Group3/wordpress.sh
@@ -12,17 +12,4 @@ sudo chown -R apache:apache /var/www/html/
 sudo systemctl restart httpd
 sudo systemctl enable httpd
 
-# #!/bin/bash
-# sudo yum update -y
-# sudo yum install httpd wget unzip epel-release mysql -y 
-# sudo yum install https://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
-# sudo yum-config-manager --enable remi-php74 
-# # sudo yum install php -y 
-# sudo yum install php php-mysql -y
-# sudo wget https://wordpress.org/latest.tar.gz tar -xf latest.tar.gz -C /var/www/html/ 
-# sudo mv /var/www/html/wordpress/* /var/www/html/ 
-# sudo chown -R apache:apache /var/www/html/
-# sudo systemctl start httpd
-# sudo systemctl enable httpd
-
 
